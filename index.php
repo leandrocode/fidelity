7f68d667093cf3e1035a09150a94985a65e9eff3
Leandro Alves <leandroalves.web@gmail.com>
2018-09-14 17:47:51 -0300

    inicio css login

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
+
+<!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>Sistema de Login</title>
+
+  <link rel="stylesheet" type="text/css" href="css/style.css>"
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
-<body> 
+<body>
+
+ <div class="teste">
   <form>
     <div class="form-group">
-      <label for="exampleInputEmail1">E-mail</label>
-      <input type="email" class="form-control" id ="exampleInputEmail1" placeholder="Digite seu E-mail">
+      <label for="Email1">E-mail</label>
+      <input type="email" class="form-control" id ="Email1" placeholder="Digite seu E-mail">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Senha</label>
       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua Senha">
     </div>
-    <div class="form-group">
-      <label for="exampleInputFile">File input</label>
-      <input type="file" id="exampleInputFile">
-      <p class="help-block">Example block-level help text here.</p>
-    </div>
-    <div class="checkbox">
-      <label>
-        <input type="checkbox"> Check me out
-      </label>
-    </div>
+
+
     <button type="submit" class="btn btn-default">Entrar</button>
   </form>
+
+</div>
+
 </body>
 </html>
