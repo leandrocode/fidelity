76febfd39afe81471814926b58403341b298b513
Leandro Alves <leandroalves.web@gmail.com>
2018-09-15 01:47:26 -0300

    cadastro e conexão com BD funcionando

--- register-form.php
+++ register-form.php
@@ -1,29 +1,58 @@
 <?php include('header.php'); ?>
 
+<?php
+
+	$connect = mysqli_connect("localhost", "root", "", "fidelity");
+	session_start();
+
+	if(isset($_POST["register"]))
+	{
+		if(empty($_POST["email"]) || empty($_POST["password"]) || empty($_POST["username"]))
+		{
+			print_r("E-mail e Senha são obrigatórios!");
+		}
+		else
+		{
+			$email = mysqli_real_escape_string($connect, $_POST["email"]);
+			$username = mysqli_real_escape_string($connect, $_POST["username"]);
+			$password = mysqli_real_escape_string($connect, $_POST["password"]);
+			$password = md5($password);
+
+			$query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
+			
+			if(mysqli_query($connect, $query))
+			{
+				print_r("Cadastro Concluído!");
+			}
+		}
+		
+	}
+?>
+
 <div class="register">
-	<form>
+	<form method="POST">
 		
 		<div class="form-group">
-			<label for="name">Nome</label>
-			<input type="text" class="form-control" id="name" name="name">
+			<label for="username">Nome</label>
+			<input type="text" class="form-control" id="username" name="username" required>
 		</div>
 
 		<div class="form-group">
 			<label for="email">E-mail</label>
-			<input type="email" class="form-control" id ="email" name="email">
+			<input type="email" class="form-control" id ="email" name="email" required>
 		</div>
 
 		<div class="form-group">
 			<label for="password">Digite sua Senha</label>
-			<input type="password" class="form-control" id="password" name="password">
+			<input type="password" class="form-control" id="password" name="password" required>
 		</div>
 
 		<div class="form-group">
 			<label for="confirmPassword">Confirme sua Senha</label>
-			<input type="confirmPassword" class="form-control" id="confirmPassword" name="confirmPassword">
+			<input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
 		</div>
 
-		<button type="submit" class="btn btn-success">Cadastrar</button>
+		<button type="submit" name="register" class="btn btn-success">Cadastrar</button>
 	</form>
 </div>
 
