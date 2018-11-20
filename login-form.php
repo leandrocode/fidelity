3d5dff201d31b6ac9829ba336272c7239627a309
Leandro Alves <leandroalves.web@gmail.com>
2018-11-20 00:51:06 -0200

    corrigindo login - method post

--- login-form.php
+++ login-form.php
@@ -14,12 +14,12 @@ if(isset($_POST['btn-entrar'])):
 		$erros[] = "<li> O campo email/senha precisa ser preenchido </li>";
 	else:
 	
-		$sql = "SELECT login FROM users WHERE login = '$email'";
+		$sql = "SELECT email FROM users WHERE email = '$email'";
 		$resultado = mysqli_query($connect, $sql);		
 
 		if(mysqli_num_rows($resultado) > 0):
 		$password = md5($password);       
-		$sql = "SELECT * FROM users WHERE login = '$email' AND senha = '$password'";
+		$sql = "SELECT * FROM users WHERE email = '$email' AND senha = '$password'";
 
 		$resultado = mysqli_query($connect, $sql);
 
@@ -46,11 +46,11 @@ endif;
 <div class="login">
 
 <br>
-	<form>
+	<form method="POST">
 	
 		<div class="form-group">
 			<label for="email">E-mail</label>
-			<input type="email" class="form-control" id ="email" name="email" placeholder="Digite seu E-mail">
+			<input type="email" class="form-control" id="email" name="email" placeholder="Digite seu E-mail">
 		</div>
 
 		<div class="form-group">
